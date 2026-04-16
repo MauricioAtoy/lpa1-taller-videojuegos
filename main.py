15255bd51978403d3811cc95a10bcceee09370be
Mauricio Atoy <javierllanosatoy@gmail.com>
2026-04-15 21:49:12 -0500

    se crea la clase juego

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 # programa principal
+class Juego:
+    def __init__(self):
+        self.estado = "menu"  # menu, combate, dialogo, tienda
+        self.jugador = Jugador()
+        self.enemigos = []
+    
+    def actualizar(self):
+        pass
+    
+    def dibujar(self):
+        pass
+
+class personaje
